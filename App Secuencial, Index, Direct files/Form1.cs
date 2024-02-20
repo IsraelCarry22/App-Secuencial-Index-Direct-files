97e60b46ff664130786a44152b77058cc0258d0a
Israel Moncada <Israelcarreon992@gmail.com>
2024-02-20 08:23:01 -0600

    ...

--- App Secuencial, Index, Direct files/Form1.cs
+++ App Secuencial, Index, Direct files/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace App_Secuencial__Index__Direct_files
